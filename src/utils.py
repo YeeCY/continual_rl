127ddb0b292017bd725f5a9a4ab8aa7e17ec1e10
YeeCY <chongyeezheng@gmail.com>
2021-02-28 22:12:36 +0800

    reformat src/utils.py and update ReplayBuffer to save memory

--- src/utils.py
+++ src/utils.py
@@ -45,6 +45,7 @@ def make_dir(dir_path):
 
 class ReplayBuffer(object):
     """Buffer to store environment transitions"""
+
     def __init__(self, obs_shape, action_shape, capacity, batch_size):
         self.capacity = capacity
         self.batch_size = batch_size
@@ -52,21 +53,35 @@ class ReplayBuffer(object):
         # the proprioceptive obs is stored as float32, pixels obs as uint8
         obs_dtype = np.float32 if len(obs_shape) == 1 else np.uint8
 
-        self.obses = np.empty((capacity, *obs_shape), dtype=obs_dtype)
-        self.next_obses = np.empty((capacity, *obs_shape), dtype=obs_dtype)
-        self.actions = np.empty((capacity, *action_shape), dtype=np.float32)
-        self.rewards = np.empty((capacity, 1), dtype=np.float32)
-        self.not_dones = np.empty((capacity, 1), dtype=np.float32)
+        # (chongyi zheng): create 0 size buffer instead of max_size buffer to save memory
+        # self.obses = np.empty((capacity, *obs_shape), dtype=obs_dtype)
+        # self.next_obses = np.empty((capacity, *obs_shape), dtype=obs_dtype)
+        # self.actions = np.empty((capacity, *action_shape), dtype=np.float32)
+        # self.rewards = np.empty((capacity, 1), dtype=np.float32)
+        # self.not_dones = np.empty((capacity, 1), dtype=np.float32)
+        self.obses = np.empty((0, *obs_shape), dtype=obs_dtype)
+        self.next_obses = np.empty((0, *obs_shape), dtype=obs_dtype)
+        self.actions = np.empty((0, *action_shape), dtype=np.float32)
+        self.rewards = np.empty((0, 1), dtype=np.float32)
+        self.not_dones = np.empty((0, 1), dtype=np.float32)
 
         self.idx = 0
         self.full = False
 
     def add(self, obs, action, reward, next_obs, done):
-        np.copyto(self.obses[self.idx], obs)
-        np.copyto(self.actions[self.idx], action)
-        np.copyto(self.rewards[self.idx], reward)
-        np.copyto(self.next_obses[self.idx], next_obs)
-        np.copyto(self.not_dones[self.idx], not done)
+        # (chongyi zheng): append transition to buffer when it is not full, replace the last recent one otherwise
+        if not self.full:
+            self.obses = np.append(self.obses, obs, axis=0)
+            self.actions = np.append(self.actions, action, axis=0)
+            self.rewards = np.append(self.rewards, reward, axis=0)
+            self.next_obses = np.append(self.next_obses, next_obs, axis=0)
+            self.not_dones = np.append(self.not_dones, not done, axis=0)
+        else:
+            np.copyto(self.obses[self.idx], obs)
+            np.copyto(self.actions[self.idx], action)
+            np.copyto(self.rewards[self.idx], reward)
+            np.copyto(self.next_obses[self.idx], next_obs)
+            np.copyto(self.not_dones[self.idx], not done)
 
         self.idx = (self.idx + 1) % self.capacity
         self.full = self.full or self.idx == 0
@@ -103,138 +118,138 @@ class ReplayBuffer(object):
         obses = random_crop(obses)
         next_obses = random_crop(next_obses)
         pos = random_crop(pos)
-        
+
         curl_kwargs = dict(obs_anchor=obses, obs_pos=pos,
-                          time_anchor=None, time_pos=None)
+                           time_anchor=None, time_pos=None)
 
         return obses, actions, rewards, next_obses, not_dones, curl_kwargs
 
 
 def get_curl_pos_neg(obs, replay_buffer):
-	"""Returns one positive pair + batch of negative samples from buffer"""
-	obs = torch.as_tensor(obs).cuda().float().unsqueeze(0)
-	pos = obs.clone()
+    """Returns one positive pair + batch of negative samples from buffer"""
+    obs = torch.as_tensor(obs).cuda().float().unsqueeze(0)
+    pos = obs.clone()
 
-	obs = random_crop(obs)
-	pos = random_crop(pos)
+    obs = random_crop(obs)
+    pos = random_crop(pos)
 
-	# Sample negatives and insert positive sample
-	obs_pos = replay_buffer.sample_curl()[-1]['obs_pos']	
-	obs_pos[0] = pos
+    # Sample negatives and insert positive sample
+    obs_pos = replay_buffer.sample_curl()[-1]['obs_pos']
+    obs_pos[0] = pos
 
-	return obs, obs_pos
+    return obs, obs_pos
 
 
 def batch_from_obs(obs, batch_size=32):
-	"""Converts a pixel obs (C,H,W) to a batch (B,C,H,W) of given size"""
-	if isinstance(obs, torch.Tensor):
-		if len(obs.shape)==3:
-			obs = obs.unsqueeze(0)
-		return obs.repeat(batch_size, 1, 1, 1)
+    """Converts a pixel obs (C,H,W) to a batch (B,C,H,W) of given size"""
+    if isinstance(obs, torch.Tensor):
+        if len(obs.shape) == 3:
+            obs = obs.unsqueeze(0)
+        return obs.repeat(batch_size, 1, 1, 1)
 
-	if len(obs.shape)==3:
-		obs = np.expand_dims(obs, axis=0)
-	return np.repeat(obs, repeats=batch_size, axis=0)
+    if len(obs.shape) == 3:
+        obs = np.expand_dims(obs, axis=0)
+    return np.repeat(obs, repeats=batch_size, axis=0)
 
 
 def _rotate_single_with_label(x, label):
-	"""Rotate an image"""
-	if label == 1:
-		return x.flip(2).transpose(1, 2)
-	elif label == 2:
-		return x.flip(2).flip(1)
-	elif label == 3:
-		return x.transpose(1, 2).flip(2)
-	return x
+    """Rotate an image"""
+    if label == 1:
+        return x.flip(2).transpose(1, 2)
+    elif label == 2:
+        return x.flip(2).flip(1)
+    elif label == 3:
+        return x.transpose(1, 2).flip(2)
+    return x
 
 
 def rotate(x):
-	"""Randomly rotate a batch of images and return labels"""
-	images = []
-	labels = torch.randint(4, (x.size(0),), dtype=torch.long).to(x.device)
-	for img, label in zip(x, labels):
-		img = _rotate_single_with_label(img, label)
-		images.append(img.unsqueeze(0))
+    """Randomly rotate a batch of images and return labels"""
+    images = []
+    labels = torch.randint(4, (x.size(0),), dtype=torch.long).to(x.device)
+    for img, label in zip(x, labels):
+        img = _rotate_single_with_label(img, label)
+        images.append(img.unsqueeze(0))
 
-	return torch.cat(images), labels
+    return torch.cat(images), labels
 
 
 def random_crop_cuda(x, size=84, w1=None, h1=None, return_w1_h1=False):
-	"""Vectorized CUDA implementation of random crop"""
-	assert isinstance(x, torch.Tensor) and x.is_cuda, \
-		'input must be CUDA tensor'
-	
-	n = x.shape[0]
-	img_size = x.shape[-1]
-	crop_max = img_size - size
+    """Vectorized CUDA implementation of random crop"""
+    assert isinstance(x, torch.Tensor) and x.is_cuda, \
+        'input must be CUDA tensor'
 
-	if crop_max <= 0:
-		if return_w1_h1:
-			return x, None, None
-		return x
+    n = x.shape[0]
+    img_size = x.shape[-1]
+    crop_max = img_size - size
 
-	x = x.permute(0, 2, 3, 1)
+    if crop_max <= 0:
+        if return_w1_h1:
+            return x, None, None
+        return x
 
-	if w1 is None:
-		w1 = torch.LongTensor(n).random_(0, crop_max)
-		h1 = torch.LongTensor(n).random_(0, crop_max)
+    x = x.permute(0, 2, 3, 1)
 
-	windows = view_as_windows_cuda(x, (1, size, size, 1))[..., 0,:,:, 0]
-	cropped = windows[torch.arange(n), w1, h1]
+    if w1 is None:
+        w1 = torch.LongTensor(n).random_(0, crop_max)
+        h1 = torch.LongTensor(n).random_(0, crop_max)
 
-	if return_w1_h1:
-		return cropped, w1, h1
+    windows = view_as_windows_cuda(x, (1, size, size, 1))[..., 0, :, :, 0]
+    cropped = windows[torch.arange(n), w1, h1]
 
-	return cropped
+    if return_w1_h1:
+        return cropped, w1, h1
+
+    return cropped
 
 
 def view_as_windows_cuda(x, window_shape):
-	"""PyTorch CUDA-enabled implementation of view_as_windows"""
-	assert isinstance(window_shape, tuple) and len(window_shape) == len(x.shape), \
-		'window_shape must be a tuple with same number of dimensions as x'
-	
-	slices = tuple(slice(None, None, st) for st in torch.ones(4).long())
-	win_indices_shape = [
-		x.size(0),
-		x.size(1)-int(window_shape[1]),
-		x.size(2)-int(window_shape[2]),
-		x.size(3)    
-	]
+    """PyTorch CUDA-enabled implementation of view_as_windows"""
+    assert isinstance(window_shape, tuple) and len(window_shape) == len(x.shape), \
+        'window_shape must be a tuple with same number of dimensions as x'
 
-	new_shape = tuple(list(win_indices_shape) + list(window_shape))
-	strides = tuple(list(x[slices].stride()) + list(x.stride()))
+    slices = tuple(slice(None, None, st) for st in torch.ones(4).long())
+    win_indices_shape = [
+        x.size(0),
+        x.size(1) - int(window_shape[1]),
+        x.size(2) - int(window_shape[2]),
+        x.size(3)
+    ]
 
-	return x.as_strided(new_shape, strides)
+    new_shape = tuple(list(win_indices_shape) + list(window_shape))
+    strides = tuple(list(x[slices].stride()) + list(x.stride()))
+
+    return x.as_strided(new_shape, strides)
 
 
 def random_crop(imgs, size=84, w1=None, h1=None, return_w1_h1=False):
-	"""Vectorized random crop, imgs: (B,C,H,W), size: output size"""
-	assert (w1 is None and h1 is None) or (w1 is not None and h1 is not None), \
-		'must either specify both w1 and h1 or neither of them'
+    """Vectorized random crop, imgs: (B,C,H,W), size: output size"""
+    assert (w1 is None and h1 is None) or (w1 is not None and h1 is not None), \
+        'must either specify both w1 and h1 or neither of them'
 
-	is_tensor = isinstance(imgs, torch.Tensor)
-	if is_tensor:
-		assert imgs.is_cuda, 'input images are tensors but not cuda!'
-		return random_crop_cuda(imgs, size=size, w1=w1, h1=h1, return_w1_h1=return_w1_h1)
-		
-	n = imgs.shape[0]
-	img_size = imgs.shape[-1]
-	crop_max = img_size - size
+    is_tensor = isinstance(imgs, torch.Tensor)
+    if is_tensor:
+        assert imgs.is_cuda, 'input images are tensors but not cuda!'
+        return random_crop_cuda(imgs, size=size, w1=w1, h1=h1, return_w1_h1=return_w1_h1)
 
-	if crop_max <= 0:
-		if return_w1_h1:
-			return imgs, None, None
-		return imgs
+    n = imgs.shape[0]
+    img_size = imgs.shape[-1]
+    crop_max = img_size - size
 
-	imgs = np.transpose(imgs, (0, 2, 3, 1))
-	if w1 is None:
-		w1 = np.random.randint(0, crop_max, n)
-		h1 = np.random.randint(0, crop_max, n)
+    if crop_max <= 0:
+        if return_w1_h1:
+            return imgs, None, None
+        return imgs
 
-	windows = view_as_windows(imgs, (1, size, size, 1))[..., 0,:,:, 0]
-	cropped = windows[np.arange(n), w1, h1]
+    imgs = np.transpose(imgs, (0, 2, 3, 1))
+    if w1 is None:
+        w1 = np.random.randint(0, crop_max, n)
+        h1 = np.random.randint(0, crop_max, n)
 
-	if return_w1_h1:
-		return cropped, w1, h1
+    windows = view_as_windows(imgs, (1, size, size, 1))[..., 0, :, :, 0]
+    cropped = windows[np.arange(n), w1, h1]
 
-	return cropped
+    if return_w1_h1:
+        return cropped, w1, h1
+
+    return cropped
