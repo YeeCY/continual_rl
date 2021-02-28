60695c842eee53c50294da8a954ed05a5d70c826
YeeCY <chongyeezheng@gmail.com>
2021-02-28 15:10:59 +0800

    walker environment demo

--- src/env/dm_control/dm_control/locomotion/tutorials/dm_control_locomotion_demo.py
+++ src/env/dm_control/dm_control/locomotion/tutorials/dm_control_locomotion_demo.py
@@ -12,7 +12,7 @@ _CONTROL_TIMESTEP = .02
 _PHYSICS_TIMESTEP = 0.005
 
 
-def ant_run(randon_state=None):
+def ant_run(random_state=None):
     walker = ant.Ant()
     arena = corr_arenas.EmptyCorridor()
     task = corr_tasks.RunThroughCorridor(
@@ -26,11 +26,11 @@ def ant_run(randon_state=None):
     return composer.Environment(
         time_limit=30,
         task=task,
-        random_state=randon_state,
+        random_state=random_state,
         strip_singleton_obs_buffer_dim=True)
 
 
-def ant_run_walls(randon_state=None):
+def ant_run_walls(random_state=None):
     walker = ant.Ant()
 
     arena = corr_arenas.WallsCorridor(
@@ -52,11 +52,11 @@ def ant_run_walls(randon_state=None):
     return composer.Environment(
         time_limit=30,
         task=task,
-        random_state=randon_state,
+        random_state=random_state,
         strip_singleton_obs_buffer_dim=True)
 
 
-def rolling_ball_with_head_run(randon_state=None):
+def rolling_ball_with_head_run(random_state=None):
     walker = jumping_ball.RollingBallWithHead()
     arena = corr_arenas.EmptyCorridor()
     task = corr_tasks.RunThroughCorridor(
@@ -70,11 +70,11 @@ def rolling_ball_with_head_run(randon_state=None):
     return composer.Environment(
         time_limit=30,
         task=task,
-        random_state=randon_state,
+        random_state=random_state,
         strip_singleton_obs_buffer_dim=True)
 
 
-def jumping_ball_with_head_run(randon_state=None):
+def jumping_ball_with_head_run(random_state=None):
     walker = jumping_ball.JumpingBallWithHead()
     arena = corr_arenas.EmptyCorridor()
     task = corr_tasks.RunThroughCorridor(
@@ -88,11 +88,11 @@ def jumping_ball_with_head_run(randon_state=None):
     return composer.Environment(
         time_limit=30,
         task=task,
-        random_state=randon_state,
+        random_state=random_state,
         strip_singleton_obs_buffer_dim=True)
 
 
-def walker_run(randon_state=None):
+def walker_run(random_state=None):
     walker = Walker()
     arena = corr_arenas.EmptyCorridor()
     task = corr_tasks.RunThroughCorridor(
@@ -106,8 +106,62 @@ def walker_run(randon_state=None):
     return composer.Environment(
         time_limit=30,
         task=task,
-        random_state=randon_state,
+        random_state=random_state,
         strip_singleton_obs_buffer_dim=True)
+
+
+def walker_run_walls(random_state=None):
+    walker = Walker()
+
+    arena = corr_arenas.WallsCorridor(
+        wall_gap=4.,
+        wall_width=distributions.Uniform(1, 7),
+        wall_height=3.0,
+        corridor_width=10,
+        corridor_length=100,
+        include_initial_padding=False)
+
+    task = corr_tasks.RunThroughCorridor(
+        walker=walker,
+        arena=arena,
+        walker_spawn_position=(0.5, 0, 0),
+        walker_spawn_rotation=0,
+        physics_timestep=_PHYSICS_TIMESTEP,
+        control_timestep=_CONTROL_TIMESTEP)
+
+    return composer.Environment(
+        time_limit=30,
+        task=task,
+        random_state=random_state,
+        strip_singleton_obs_buffer_dim=True)
+
+
+def walker_run_gaps(random_state=None):
+    walker = Walker()
+
+    # Build a corridor-shaped arena with gaps, where the sizes of the gaps and
+    # platforms are uniformly randomized.
+    arena = corr_arenas.GapsCorridor(
+        platform_length=distributions.Uniform(.3, 2.5),
+        gap_length=distributions.Uniform(.5, 1.25),
+        corridor_width=10,
+        corridor_length=100)
+
+    # Build a task that rewards the agent for running down the corridor at a
+    # specific velocity.
+    task = corr_tasks.RunThroughCorridor(
+        walker=walker,
+        arena=arena,
+        walker_spawn_position=(0.5, 0, 0),
+        target_velocity=3.0,
+        physics_timestep=0.005,
+        control_timestep=0.03)
+
+    return composer.Environment(time_limit=30,
+                                task=task,
+                                random_state=random_state,
+                                strip_singleton_obs_buffer_dim=True)
+
 
 def main():
     # viewer.launch(environment_loader=ant_run)
@@ -116,7 +170,9 @@ def main():
     # viewer.launch(environment_loader=jumping_ball_with_head_run)
     # viewer.launch(environment_loader=basic_cmu_2019.cmu_humanoid_run_walls)
     # viewer.launch(environment_loader=basic_cmu_2019.cmu_humanoid_run_gaps)
-    viewer.launch(environment_loader=walker_run)
+    # viewer.launch(environment_loader=walker_run)
+    # viewer.launch(environment_loader=walker_run_walls)
+    viewer.launch(environment_loader=walker_run_gaps)
 
 
 if __name__ == "__main__":
