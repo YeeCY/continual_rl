fix ant tracking camera rotation

src/env/dm_control/dm_control/locomotion/tutorials/dm_control_locomotion_demo.py
@@ -3,6 +3,7 @@ from dm_control import viewer
 from dm_control.composer.variation import distributions
 from dm_control.locomotion.arenas import corridors as corr_arenas
 from dm_control.locomotion.tasks import corridors as corr_tasks
+from dm_control.locomotion.examples import basic_cmu_2019
 from dm_control.locomotion.walkers import ant, jumping_ball, initializers
 from dm_control.locomotion.walkers.planar_walker import PlanarWalker
 
@@ -65,6 +66,34 @@ def ant_run_walls(random_state=None):
         arena=arena,
         walker_spawn_position=(0.5, 0, 0),
         walker_spawn_rotation=0,
+        physics_timestep=_PHYSICS_TIMESTEP,
+        control_timestep=_CONTROL_TIMESTEP)
+
+    return composer.Environment(
+        time_limit=30,
+        task=task,
+        random_state=random_state,
+        strip_singleton_obs_buffer_dim=True)
+
+
+def ant_run_gaps(random_state=None):
+    walker = ant.Ant()
+
+    # Build a corridor-shaped arena with gaps, where the sizes of the gaps and
+    # platforms are uniformly randomized.
+    arena = corr_arenas.GapsCorridor(
+        platform_length=distributions.Uniform(.3, 2.5),
+        gap_length=distributions.Uniform(.5, 1.0),
+        corridor_width=10,
+        corridor_length=100)
+
+    # Build a task that rewards the agent for running down the corridor at a
+    # specific velocity.
+    task = corr_tasks.RunThroughCorridor(
+        walker=walker,
+        arena=arena,
+        walker_spawn_position=(1.0, 0, 0),
+        target_velocity=3.0,
         physics_timestep=_PHYSICS_TIMESTEP,
         control_timestep=_CONTROL_TIMESTEP)
 
@@ -180,6 +209,7 @@ def main():
     # viewer.launch(environment_loader=ant_run)
     # viewer.launch(ant_run_long)
     # viewer.launch(environment_loader=ant_run_walls)
+    viewer.launch(environment_loader=ant_run_gaps)
     # viewer.launch(environment_loader=rolling_ball_with_head_run)
     # viewer.launch(environment_loader=jumping_ball_with_head_run)
     # viewer.launch(environment_loader=basic_cmu_2019.cmu_humanoid_run_walls)
@@ -187,23 +217,23 @@ def main():
     # viewer.launch(environment_loader=walker_run)
     # viewer.launch(environment_loader=walker_run_gaps)
 
-    # Build an example environment.
-    import numpy as np
-
-    env = ant_run_long()
-
-    # Get the `action_spec` describing the control inputs.
-    action_spec = env.action_spec()
-
-    # Step through the environment for one episode with random actions.
-    time_step = env.reset()
-    while not time_step.last():
-        action = np.random.uniform(action_spec.minimum, action_spec.maximum,
-                                   size=action_spec.shape)
-        time_step = env.step(action)
-        print("reward = {}, discount = {}, observations = {}.".format(
-            time_step.reward, time_step.discount, time_step.observation))
-
+    # # Build an example environment.
+    # import numpy as np
+    #
+    # env = ant_run_long()
+    #
+    # # Get the `action_spec` describing the control inputs.
+    # action_spec = env.action_spec()
+    #
+    # # Step through the environment for one episode with random actions.
+    # time_step = env.reset()
+    # while not time_step.last():
+    #     action = np.random.uniform(action_spec.minimum, action_spec.maximum,
+    #                                size=action_spec.shape)
+    #     time_step = env.step(action)
+    #     print("reward = {}, discount = {}, observations = {}.".format(
+    #         time_step.reward, time_step.discount, time_step.observation))
+    #
     # print("done")
 
 

src/env/wrappers.py
@@ -49,14 +49,18 @@ def make_locomotion_env(
         episode_length=1000,
         frame_stack=3,
         action_repeat=4,
+        obs_height=100,
+        obs_width=100,
+        camera_id=0,
         mode='train'):
     """(chongyi zheng) Make dm_control locomotion environments for experiments"""
     env = dmc2gym.make_locomotion(
         env_name=env_name,
         seed=seed,
         from_pixels=True,
-        height=100,
-        width=100,
+        height=obs_height,
+        width=obs_width,
+        camera_id=camera_id,
         episode_length=episode_length,
         frame_skip=action_repeat
     )
@@ -380,7 +384,7 @@ class VideoBackground(gym.Wrapper):
         obs, reward, done, info = self.env.step(action)
         return self._change_bg(obs), reward, done, info
 
-    def _change_bg(self, obs):
+    def _change_bg(self, obs, camera_id=None):
         """Applies greenscreen if video is selected, otherwise does nothing"""
         if self._video:
             bg = self._data[self._current_frame % len(self._data)]  # select frame
@@ -389,22 +393,22 @@ class VideoBackground(gym.Wrapper):
             dmc_loco_wrapper = self.env.env
             seg = self.env.render(
                 mode='segmentation',
-                height=dmc_loco_wrapper.height,
-                width=dmc_loco_wrapper.width,
-                camera_id=dmc_loco_wrapper.camera_id
+                height=obs.shape[1],
+                width=obs.shape[2],
+                camera_id=dmc_loco_wrapper.camera_id if not camera_id else camera_id
             )
             seg = seg.transpose(2, 0, 1)  # channels first
 
             return replace_bg(obs, seg, bg)
         return obs
 
-    def apply_to(self, obs):
+    def apply_to(self, obs, camera_id=None):
         """Applies greenscreen mode of object to observation"""
         obs = obs.copy()
         channels_last = obs.shape[-1] == 3
         if channels_last:
             obs = torch.from_numpy(obs).permute(2, 0, 1).numpy()
-        obs = self._change_bg(obs)
+        obs = self._change_bg(obs, camera_id=camera_id)
         if channels_last:
             obs = torch.from_numpy(obs).permute(1, 2, 0).numpy()
         return obs