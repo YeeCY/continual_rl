9161245b2b0ab099aafec09a035bdac448e424c3
YeeCY <chongyeezheng@gmail.com>
2021-05-02 23:42:27 +0800

    adding agem_classifier.py

--- src/mnist_cl/agem_classifier.py
+++ src/mnist_cl/agem_classifier.py
@@ -2,6 +2,8 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 from torch import optim
+import numpy as np
+
 import utils
 
 
@@ -42,6 +44,10 @@ class AgemClassifier(nn.Module):
     def forward(self, x):
         return self.trunk(x)
 
+    def reduce_exemplar_sets(self, m):
+        for y, p_y in enumerate(self.exemplar_sets):
+            self.exemplar_sets[y] = p_y[:m]
+
     def construct_exemplar_set(self, dataset, n):
         # set model to eval()-mode
         mode = self.training
@@ -50,51 +56,51 @@ class AgemClassifier(nn.Module):
         n_max = len(dataset)
         exemplar_set = []
 
-        if self.herding:
-            # compute features for each example in [dataset]
-            first_entry = True
-            dataloader = utils.get_data_loader(dataset, 128, cuda=self._is_on_cuda())
-            for (image_batch, _) in dataloader:
-                image_batch = image_batch.to(self._device())
-                with torch.no_grad():
-                    feature_batch = self.feature_extractor(image_batch).cpu()
-                if first_entry:
-                    features = feature_batch
-                    first_entry = False
-                else:
-                    features = torch.cat([features, feature_batch], dim=0)
-            if self.norm_exemplars:
-                features = F.normalize(features, p=2, dim=1)
-
-            # calculate mean of all features
-            class_mean = torch.mean(features, dim=0, keepdim=True)
-            if self.norm_exemplars:
-                class_mean = F.normalize(class_mean, p=2, dim=1)
-
-            # one by one, select exemplar that makes mean of all exemplars as close to [class_mean] as possible
-            exemplar_features = torch.zeros_like(features[:min(n, n_max)])
-            list_of_selected = []
-            for k in range(min(n, n_max)):
-                if k>0:
-                    exemplar_sum = torch.sum(exemplar_features[:k], dim=0).unsqueeze(0)
-                    features_means = (features + exemplar_sum)/(k+1)
-                    features_dists = features_means - class_mean
-                else:
-                    features_dists = features - class_mean
-                index_selected = np.argmin(torch.norm(features_dists, p=2, dim=1))
-                if index_selected in list_of_selected:
-                    raise ValueError("Exemplars should not be repeated!!!!")
-                list_of_selected.append(index_selected)
-
-                exemplar_set.append(dataset[index_selected][0].numpy())
-                exemplar_features[k] = copy.deepcopy(features[index_selected])
-
-                # make sure this example won't be selected again
-                features[index_selected] = features[index_selected] + 10000
-        else:
-            indeces_selected = np.random.choice(n_max, size=min(n, n_max), replace=False)
-            for k in indeces_selected:
-                exemplar_set.append(dataset[k][0].numpy())
+        # if self.herding:
+        #     # compute features for each example in [dataset]
+        #     first_entry = True
+        #     dataloader = utils.get_data_loader(dataset, 128, cuda=self._is_on_cuda())
+        #     for (image_batch, _) in dataloader:
+        #         image_batch = image_batch.to(self._device())
+        #         with torch.no_grad():
+        #             feature_batch = self.feature_extractor(image_batch).cpu()
+        #         if first_entry:
+        #             features = feature_batch
+        #             first_entry = False
+        #         else:
+        #             features = torch.cat([features, feature_batch], dim=0)
+        #     if self.norm_exemplars:
+        #         features = F.normalize(features, p=2, dim=1)
+        #
+        #     # calculate mean of all features
+        #     class_mean = torch.mean(features, dim=0, keepdim=True)
+        #     if self.norm_exemplars:
+        #         class_mean = F.normalize(class_mean, p=2, dim=1)
+        #
+        #     # one by one, select exemplar that makes mean of all exemplars as close to [class_mean] as possible
+        #     exemplar_features = torch.zeros_like(features[:min(n, n_max)])
+        #     list_of_selected = []
+        #     for k in range(min(n, n_max)):
+        #         if k>0:
+        #             exemplar_sum = torch.sum(exemplar_features[:k], dim=0).unsqueeze(0)
+        #             features_means = (features + exemplar_sum)/(k+1)
+        #             features_dists = features_means - class_mean
+        #         else:
+        #             features_dists = features - class_mean
+        #         index_selected = np.argmin(torch.norm(features_dists, p=2, dim=1))
+        #         if index_selected in list_of_selected:
+        #             raise ValueError("Exemplars should not be repeated!!!!")
+        #         list_of_selected.append(index_selected)
+        #
+        #         exemplar_set.append(dataset[index_selected][0].numpy())
+        #         exemplar_features[k] = copy.deepcopy(features[index_selected])
+        #
+        #         # make sure this example won't be selected again
+        #         features[index_selected] = features[index_selected] + 10000
+        # else:
+        indeces_selected = np.random.choice(n_max, size=min(n, n_max), replace=False)
+        for k in indeces_selected:
+            exemplar_set.append(dataset[k][0].numpy())
 
         # add this [exemplar_set] as a [n]x[ich]x[isz]x[isz] to the list of [exemplar_sets]
         self.exemplar_sets.append(np.array(exemplar_set))
@@ -102,15 +108,12 @@ class AgemClassifier(nn.Module):
         # set mode of model back
         self.train(mode=mode)
 
-    def train_a_batch(self, x, y, scores=None, x_=None, y_=None, scores_=None, rnt=0.5, active_classes=None, task=1):
+    def train_a_batch(self, x, y, x_=None, y_=None, active_classes=None):
         # Set model to training-mode
         self.train()
 
         # Reset optimizer
         self.optimizer.zero_grad()
-
-        # Should gradient be computed separately for each task? (needed when a task-mask is combined with replay)
-        gradient_per_task = True if ((self.mask_dict is not None) and (x_ is not None)) else False
 
 
         ##--(1)-- REPLAYED DATA --##
@@ -118,30 +121,24 @@ class AgemClassifier(nn.Module):
         if x_ is not None:
             # In the Task-IL scenario, [y_] or [scores_] is a list and [x_] needs to be evaluated on each of them
             # (in case of 'exact' or 'exemplar' replay, [x_] is also a list!
-            TaskIL = (type(y_)==list) if (y_ is not None) else (type(scores_)==list)
-            if not TaskIL:
-                y_ = [y_]
-                scores_ = [scores_]
-                active_classes = [active_classes] if (active_classes is not None) else None
-            n_replays = len(y_) if (y_ is not None) else len(scores_)
+            y_ = [y_]
+            active_classes = [active_classes] if (active_classes is not None) else None
+            n_replays = len(y_) if (y_ is not None) else None
 
             # Prepare lists to store losses for each replay
             loss_replay = [None]*n_replays
             predL_r = [None]*n_replays
-            distilL_r = [None]*n_replays
 
             # Run model (if [x_] is not a list with separate replay per task and there is no task-specific mask)
-            if (not type(x_)==list) and (self.mask_dict is None):
+            if not isinstance(x_, list):
                 y_hat_all = self(x_)
 
             # Loop to evalute predictions on replay according to each previous task
             for replay_id in range(n_replays):
 
                 # -if [x_] is a list with separate replay per task, evaluate model on this task's replay
-                if (type(x_)==list) or (self.mask_dict is not None):
+                if isinstance(x_, list):
                     x_temp_ = x_[replay_id] if type(x_)==list else x_
-                    if self.mask_dict is not None:
-                        self.apply_XdGmask(task=replay_id+1)
                     y_hat_all = self(x_temp_)
 
                 # -if needed (e.g., Task-IL or Class-IL scenario), remove predictions for classes not in replayed task
@@ -149,39 +146,16 @@ class AgemClassifier(nn.Module):
 
                 # Calculate losses
                 if (y_ is not None) and (y_[replay_id] is not None):
-                    if self.binaryCE:
-                        binary_targets_ = utils.to_one_hot(y_[replay_id].cpu(), y_hat.size(1)).to(y_[replay_id].device)
-                        predL_r[replay_id] = F.binary_cross_entropy_with_logits(
-                            input=y_hat, target=binary_targets_, reduction='none'
-                        ).sum(dim=1).mean()     #--> sum over classes, then average over batch
-                    else:
-                        predL_r[replay_id] = F.cross_entropy(y_hat, y_[replay_id], reduction='mean')
-                if (scores_ is not None) and (scores_[replay_id] is not None):
-                    # n_classes_to_consider = scores.size(1) #--> with this version, no zeroes are added to [scores]!
-                    n_classes_to_consider = y_hat.size(1)    #--> zeros will be added to [scores] to make it this size!
-                    kd_fn = utils.loss_fn_kd_binary if self.binaryCE else utils.loss_fn_kd
-                    distilL_r[replay_id] = kd_fn(scores=y_hat[:, :n_classes_to_consider],
-                                                 target_scores=scores_[replay_id], T=self.KD_temp)
-                # Weigh losses
-                if self.replay_targets=="hard":
-                    loss_replay[replay_id] = predL_r[replay_id]
-                elif self.replay_targets=="soft":
-                    loss_replay[replay_id] = distilL_r[replay_id]
-
-                # If needed, perform backward pass before next task-mask (gradients of all tasks will be accumulated)
-                if gradient_per_task:
-                    weight = 1 if self.AGEM else (1 - rnt)
-                    weighted_replay_loss_this_task = weight * loss_replay[replay_id] / n_replays
-                    weighted_replay_loss_this_task.backward()
+                    predL_r[replay_id] = F.cross_entropy(y_hat, y_[replay_id], reduction='mean')
 
         # Calculate total replay loss
         loss_replay = None if (x_ is None) else sum(loss_replay) / n_replays
 
         # If using A-GEM, calculate and store averaged gradient of replayed data
-        if self.AGEM and x_ is not None:
+        if x_ is not None:
             # Perform backward pass to calculate gradient of replayed batch (if not yet done)
-            if not gradient_per_task:
-                loss_replay.backward()
+            loss_replay.backward()
+
             # Reorganize the gradient of the replayed batch as a single vector
             grad_rep = []
             for p in self.parameters():
@@ -191,14 +165,8 @@ class AgemClassifier(nn.Module):
             # Reset gradients (with A-GEM, gradients of replayed batch should only be used as inequality constraint)
             self.optimizer.zero_grad()
 
-
         ##--(2)-- CURRENT DATA --##
-
         if x is not None:
-            # If requested, apply correct task-specific mask
-            if self.mask_dict is not None:
-                self.apply_XdGmask(task=task)
-
             # Run model
             y_hat = self(x)
             # -if needed, remove predictions for classes not in current task
@@ -207,19 +175,8 @@ class AgemClassifier(nn.Module):
                 y_hat = y_hat[:, class_entries]
 
             # Calculate prediction loss
-            if self.binaryCE:
-                # -binary prediction loss
-                binary_targets = utils.to_one_hot(y.cpu(), y_hat.size(1)).to(y.device)
-                if self.binaryCE_distill and (scores is not None):
-                    classes_per_task = int(y_hat.size(1) / task)
-                    binary_targets = binary_targets[:, -(classes_per_task):]
-                    binary_targets = torch.cat([torch.sigmoid(scores / self.KD_temp), binary_targets], dim=1)
-                predL = None if y is None else F.binary_cross_entropy_with_logits(
-                    input=y_hat, target=binary_targets, reduction='none'
-                ).sum(dim=1).mean()     #--> sum over classes, then average over batch
-            else:
-                # -multiclass prediction loss
-                predL = None if y is None else F.cross_entropy(input=y_hat, target=y, reduction='mean')
+            # -multiclass prediction loss
+            predL = None if y is None else F.cross_entropy(input=y_hat, target=y, reduction='mean')
 
             # Weigh losses
             loss_cur = predL
@@ -227,41 +184,21 @@ class AgemClassifier(nn.Module):
             # Calculate training-precision
             precision = None if y is None else (y == y_hat.max(1)[1]).sum().item() / x.size(0)
 
-            # If backward passes are performed per task (e.g., XdG combined with replay), perform backward pass
-            if gradient_per_task:
-                weighted_current_loss = rnt*loss_cur
-                weighted_current_loss.backward()
         else:
             precision = predL = None
             # -> it's possible there is only "replay" [e.g., for offline with task-incremental learning]
 
 
         # Combine loss from current and replayed batch
-        if x_ is None or self.AGEM:
+        if x_ is None:
             loss_total = loss_cur
-        else:
-            loss_total = loss_replay if (x is None) else rnt*loss_cur+(1-rnt)*loss_replay
-
 
         ##--(3)-- ALLOCATION LOSSES --##
-
-        # Add SI-loss (Zenke et al., 2017)
-        surrogate_loss = self.surrogate_loss()
-        if self.si_c>0:
-            loss_total += self.si_c * surrogate_loss
-
-        # Add EWC-loss
-        ewc_loss = self.ewc_loss()
-        if self.ewc_lambda>0:
-            loss_total += self.ewc_lambda * ewc_loss
-
-
         # Backpropagate errors (if not yet done)
-        if not gradient_per_task:
-            loss_total.backward()
+        loss_total.backward()
 
         # If using A-GEM, potentially change gradient:
-        if self.AGEM and x_ is not None:
+        if x_ is not None:
             # -reorganize gradient (of current batch) as single vector
             grad_cur = []
             for p in self.parameters():
@@ -269,7 +206,7 @@ class AgemClassifier(nn.Module):
                     grad_cur.append(p.grad.view(-1))
             grad_cur = torch.cat(grad_cur)
             # -check inequality constrain
-            angle = (grad_cur*grad_rep).sum()
+            angle = (grad_cur * grad_rep).sum()
             if angle < 0:
                 # -if violated, project the gradient of the current batch onto the gradient of the replayed batch ...
                 length_rep = (grad_rep*grad_rep).sum()
@@ -289,11 +226,7 @@ class AgemClassifier(nn.Module):
         return {
             'loss_total': loss_total.item(),
             'loss_current': loss_cur.item() if x is not None else 0,
-            'loss_replay': loss_replay.item() if (loss_replay is not None) and (x is not None) else 0,
             'pred': predL.item() if predL is not None else 0,
-            'pred_r': sum(predL_r).item()/n_replays if (x_ is not None and predL_r[0] is not None) else 0,
-            'distil_r': sum(distilL_r).item()/n_replays if (x_ is not None and distilL_r[0] is not None) else 0,
-            'ewc': ewc_loss.item(), 'si_loss': surrogate_loss.item(),
             'precision': precision if precision is not None else 0.,
         }
 
