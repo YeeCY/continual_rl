3a61896f0df52fca5a9c79cb7219d21964835312
Chongyi Zheng <chongyeezheng@gmail.com>
2021-05-09 07:50:43 -0400

    add bash_run/train_ppo_mujoco_continual_walker2d_halfcheetah_hopper.sh

---
+++ bash_run/train_ppo_mujoco_continual_walker2d_halfcheetah_hopper_ewc.sh
@@ -0,0 +1,21 @@
+export PYTHONPATH=$HOME/policy-adaptation-during-deployment/
+export PWD=$HOME/policy-adaptation-during-deployment
+export CUDA_VISIBLE_DEVICES=0
+
+xvfb-run -a -s "-screen 0 1400x900x24" python $PWD/src/train_ppo.py \
+  --env_names Walker2d HalfCheetah-v3 Hopper-v3 \
+  --env_type mujoco \
+  --algo ewc_ppo_mlp \
+  --train_steps_per_task 1000000 \
+  --save_freq 10 \
+  --eval_freq 10 \
+  --discount 0.99 \
+  --batch_size 32 \
+  --ppo_num_rollout_steps_per_process 2048 \
+  --ppo_num_processes 1 \
+  --ppo_use_clipped_critic_loss \
+  --ppo_use_proper_time_limits \
+  --ppo_ewc_lambda 50 \
+  --seed 0 \
+  --work_dir $PWD/vec_logs/walker2d_halfcheetah_hopper_ewc/0 \
+  --save_model
