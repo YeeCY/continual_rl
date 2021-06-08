8de3acc183c16ce6f2ea9c0e941dea294e05711c
Chongyi Zheng <chongyeezheng@gmail.com>
2021-06-08 12:20:16 -0400

    add more scripts

--- bash_run/metaworld/local/test_train_mh_sac_metaworld_continual_10_tasks_sgd_seeds0123.sh
+++ bash_run/metaworld/local/test_train_mh_sac_metaworld_continual_10_tasks_sgd_seeds0123.sh
@@ -6,11 +6,10 @@ PROJECT_DIR=$(realpath "$SCRIPT_DIR/../../..")
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/.mujoco/mujoco200/bin
 export PYTHONPATH=$PROJECT_DIR
 
-declare -a seeds=(0 1 2 3)
+declare -a seeds=(0)
 
 for seed in "${seeds[@]}"; do
   export CUDA_VISIBLE_DEVICES="$seed"
-  nohup \
   python $PROJECT_DIR/src/train_sac.py \
     --env_names \
       window-close-v2 \
@@ -33,6 +32,5 @@ for seed in "${seeds[@]}"; do
     --sac_num_processes 1 \
     --sac_num_train_iters 1000 \
     --seed $seed \
-    --work_dir $PROJECT_DIR/vec_logs/mh_sac_mlp_metaworld_10_tasks/sgd/$seed \
-    > $PROJECT_DIR/terminal_logs/mh_sac_mlp_metaworld_10_tasks-sgd-seed"$seed".log 2>&1 &
+    --work_dir $PROJECT_DIR/debug_logs/mh_sac_mlp_metaworld_10_tasks/sgd/$seed
 done
