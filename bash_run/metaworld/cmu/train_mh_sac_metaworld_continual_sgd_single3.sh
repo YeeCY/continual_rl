6821bca9ca0d4de386344bb26262fbd4bc96aa00
Chongyi Zheng <chongyeezheng@gmail.com>
2021-06-12 10:08:46 -0400

    update cmu scripts

--- bash_run/metaworld/cmu/train_mh_sac_metaworld_continual_sgd_single3.sh
+++ bash_run/metaworld/cmu/train_mh_sac_metaworld_continual_sgd_single3.sh
@@ -12,17 +12,17 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/.mujoco/mujoco200/bin
 export PYTHONPATH=$PROJECT_DIR
 
 declare -a all_env_names=(
-  handle-pull-side-v2
-  handle-pull-v2
-  pick-out-of-hole-v2
-  push-back-v2
+  soccer-v2
+  sweep-into-v2
+  window-open-v2
+  reach-wall-v2
 )
 
 declare -a seeds=(1 2 3 4)
 
 for env_names in "${all_env_names[@]}"; do
   for seed in "${seeds[@]}"; do
-    export CUDA_VISIBLE_DEVICES="$(($seed - 1))"
+    export CUDA_VISIBLE_DEVICES="$(("$seed" - 4))"
     nohup \
     python $PROJECT_DIR/src/train_sac.py \
       --env_names $env_names \
