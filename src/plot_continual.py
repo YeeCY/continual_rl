837d97f347a624fb0c7df4e2b8c0b09c1104d83f
Chongyi Zheng <chongyeezheng@gmail.com>
2021-06-24 00:31:28 -0400

    add ppo_mlp_v2_overparam_metaworld_10_tasks.png

--- src/plot_continual.py
+++ src/plot_continual.py
@@ -107,6 +107,11 @@ CURVE_FORMAT = {
         'style': '-',
         'label': 'AGEM_REF_GRAD_BATCH_SIZE2048'
     },
+    'agem_ref_grad_batch_size2500': {
+        'color': [0, 153, 153],
+        'style': '-',
+        'label': 'AGEM_REF_GRAD_BATCH_SIZE2500'
+    },
     'agem_ref_grad_batch_size5000': {
         'color': [64, 64, 64],
         'style': '-',
