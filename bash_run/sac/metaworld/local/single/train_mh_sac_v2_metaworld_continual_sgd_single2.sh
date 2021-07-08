9522c961e6f0fc4dd95416e478db3d7e8dbe54dd
Chongyi Zheng <chongyeezheng@gmail.com>
2021-07-08 00:14:54 -0400

    add mh_sac_mlp_v2 single scripts

--- bash_run/sac/metaworld/local/single/train_mh_sac_v2_metaworld_continual_sgd_single2.sh
+++ bash_run/sac/metaworld/local/single/train_mh_sac_v2_metaworld_continual_sgd_single2.sh
@@ -1,40 +1,39 @@
 #!/bin/bash
 
 SCRIPT_DIR=$(dirname "$BASH_SOURCE")
-PROJECT_DIR=$(realpath "$SCRIPT_DIR/../../../..")
+PROJECT_DIR=$(realpath "$SCRIPT_DIR/../../../../..")
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/.mujoco/mujoco200/bin
 export PYTHONPATH=$PROJECT_DIR
 
 declare -a all_env_names=(
-  peg-unplug-side-v2
-  soccer-v2
-  stick-push-v2
-  stick-pull-v2
-  push-wall-v2
+  coffee-pull-v2
+  coffee-push-v2
+  disassemble-v2
+  hammer-v2
+  hand-insert-v2
 )
 
 declare -a seeds=(0 1 2 3)
 
 for env_names in "${all_env_names[@]}"; do
   for seed in "${seeds[@]}"; do
-    export CUDA_VISIBLE_DEVICES="$(("$seed" - 1))"
+    export CUDA_VISIBLE_DEVICES=$seed
     nohup \
     python $PROJECT_DIR/src/train_sac.py \
       --env_names $env_names \
       --env_type metaworld \
-      --algo mh_sac_mlp \
+      --algo mh_sac_mlp_v2 \
       --train_steps_per_task 500000 \
       --eval_freq 10 \
-      --num_eval_episodes 10 \
       --discount 0.99 \
+      --sac_actor_hidden_dim 256 \
       --sac_init_steps 1000 \
       --sac_num_expl_steps_per_process 1000 \
       --sac_num_processes 1 \
       --sac_num_train_iters 1000 \
       --seed $seed \
-      --save_video \
-      --work_dir $PROJECT_DIR/vec_logs/mh_sac_mlp_metaworld_single/sgd/$env_names/$seed \
-      > $PROJECT_DIR/terminal_logs/mh_sac_mlp_metaworld_single-sgd-"$env_names"-seed"$seed".log 2>&1 &
+      --work_dir $PROJECT_DIR/vec_logs/mh_sac_mlp_v2_metaworld_single/sgd/$env_names/$seed \
+      > $PROJECT_DIR/terminal_logs/mh_sac_mlp_v2_metaworld_single-sgd-"$env_names"-seed"$seed".log 2>&1 &
   done
 done
