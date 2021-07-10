fix ppo style gradient projection loss for actor

src/agent/network.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from itertools import chain
 
 
@@ -395,12 +396,11 @@ class MultiHeadSacActorMlp(nn.Module):
         ) * (log_std + 1)
 
         std = log_std.exp()
-        pi = (action - mu) / (std + 1e-6)
-        log_pi = gaussian_logprob(pi, log_std)
-        # log_pi = torch.distributions.Independent(
-        #     torch.distributions.Normal(mu, std), 1).log_prob(action)
+        noise = (action - mu) / (std + 1e-6)
+        log_pi = gaussian_logprob(noise, log_std)
 
-        mu, _, log_pi = squash(mu, pi, log_pi)
+        # squash log_pi
+        log_pi -= torch.log(F.relu(1 - action.pow(2)) + 1e-6).sum(-1, keepdim=True)
 
         return log_pi
 

src/agent/sac/agem_v2_mh_sac_agent_v2.py
@@ -55,11 +55,11 @@ class AgemV2MultiHeadSacMlpAgentV2(MultiHeadSacMlpAgentV2, AgemV2SacMlpAgentV2):
 
             # (chongyi zheng): use PPO style gradient projection loss for actor
             log_pis = self.actor.compute_log_probs(obses, actions, head_idx=task_id)
-            ratio = torch.exp(log_pis - old_log_pis)  # importance sampling ratio
-            proj_actor_loss = (ratio * qs).mean()
+            ratio = torch.exp(log_pis - old_log_pis.detach())  # importance sampling ratio
+            proj_actor_loss = (ratio * qs.detach()).mean()
 
             self.actor_optimizer.zero_grad()  # clear current gradient
-            proj_actor_loss.backward(retain_graph=True)
+            proj_actor_loss.backward()
 
             single_ref_actor_grad = []
             for param in self.actor.common_parameters():