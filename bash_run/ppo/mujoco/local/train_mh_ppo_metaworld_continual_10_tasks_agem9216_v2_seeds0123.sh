d83209ad87fb2bb8ca31e2ee0938fd20827fa63d
Chongyi Zheng <chongyeezheng@gmail.com>
2021-07-13 12:05:44 -0400

    add mh_sac improvement mujoco and mh_ppo_v2 mujoco scripts

---
+++ bash_run/ppo/mujoco/local/train_mh_ppo_metaworld_continual_10_tasks_agem9216_v2_seeds0123.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(dirname "$BASH_SOURCE")
+PROJECT_DIR=$(realpath "$SCRIPT_DIR/../../../..")
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/.mujoco/mujoco200/bin
+export PYTHONPATH=$PROJECT_DIR
+
+declare -a seeds=(0 1 2 3)
+
+for seed in "${seeds[@]}"; do
+  export CUDA_VISIBLE_DEVICES=$seed
+  nohup \
+  python $PROJECT_DIR/src/train_ppo.py \
+    --env_names \
+      Walker2d-v3 \
+      HalfCheetah-v3 \
+      Hopper-v3 \
+      Pusher-v2 \
+      Ant-v3 \
+      Striker-v2 \
+      InvertedPendulum-v2 \
+      Reacher-v2 \
+      Swimmer-v3 \
+      Humanoid-v3 \
+    --env_type mujoco \
+    --algo agem_mh_ppo_mlp_v2 \
+    --train_steps_per_task 1000000 \
+    --eval_freq 10 \
+    --discount 0.99 \
+    --ppo_num_rollout_steps_per_process 2048 \
+    --ppo_num_processes 1 \
+    --ppo_use_clipped_critic_loss \
+    --ppo_use_proper_time_limits \
+    --ppo_agem_memory_budget 18432 \
+    --ppo_agem_ref_grad_batch_size 9216 \
+    --seed $seed \
+    --work_dir $PROJECT_DIR/vec_logs/mh_ppo_mlp_v2_mujoco_10_tasks/agem_ref_grad_batch_size9216/$seed \
+    > $PROJECT_DIR/terminal_logs/mh_ppo_mlp_v2_mujoco_10_tasks-agem_ref_grad_batch_size9216-seed"$seed".log 2>&1 &
+done
---
+++ bash_run/sac/mujoco/local/improvement/train_mh_sac_v2_overparam256_mujoco_continual_10_tasks_improvement_agem_ref_grad_batch_size4500_seeds0123_v3.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(dirname "$BASH_SOURCE")
+PROJECT_DIR=$(realpath "$SCRIPT_DIR/../../../../..")
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/.mujoco/mujoco200/bin
+export PYTHONPATH=$PROJECT_DIR
+
+declare -a seeds=(0 1 2 3)
+
+for seed in "${seeds[@]}"; do
+  export CUDA_VISIBLE_DEVICES="$seed"
+  nohup \
+  python $PROJECT_DIR/src/train_sac.py \
+    --env_names \
+      Walker2d-v3 \
+      HalfCheetah-v3 \
+      Hopper-v3 \
+      Pusher-v2 \
+      Ant-v3 \
+      Striker-v2 \
+      InvertedPendulum-v2 \
+      Reacher-v2 \
+      Swimmer-v3 \
+      Humanoid-v3 \
+    --env_type mujoco \
+    --algo agem_v2_mh_sac_mlp_v2 \
+    --train_steps_per_task 1000000 \
+    --eval_freq 10 \
+    --discount 0.99 \
+    --sac_actor_hidden_dim 256 \
+    --sac_init_steps 2048 \
+    --sac_num_expl_steps_per_process 2048 \
+    --sac_num_processes 1 \
+    --sac_num_train_iters 2048 \
+    --sac_agem_memory_budget 18432 \
+    --sac_agem_ref_grad_batch_size 9216 \
+    --seed $seed \
+    --work_dir $PROJECT_DIR/vec_logs/mh_sac_mlp_v2_mujoco_10_tasks_improvement/agem_ref_grad_batch_size9216/$seed \
+    > $PROJECT_DIR/terminal_logs/mh_sac_mlp_v2_mujoco_10_tasks_improvement-agem_ref_grad_batch_size9216-seed"$seed".log 2>&1 &
+done
